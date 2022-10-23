20a0e596d3d857bb2b5342948f62347891bf36f3
Deepa John <116244627+ServiceofDeepa@users.noreply.github.com>
2022-10-23 00:10:45 -0700

    Update streamlit_app.py
    Removing plain text display

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.text(fruityvice_response.json())
+
 
 # Formatting the data output.. take the data version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
