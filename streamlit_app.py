6952e37ade0192b9ebfde630252729a75c5c1da3
Deepa John <116244627+ServiceofDeepa@users.noreply.github.com>
2022-10-23 00:07:15 -0700

    Update streamlit_app.py
    Added comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+# Formatting the data output
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# Display the data as a data set
 streamlit.dataframe(fruityvice_normalized)
